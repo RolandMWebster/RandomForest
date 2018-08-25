f769ebcbadd7cebefc84675ea3c006c917813d7a
roland webster <rolandmwebster@gmail.com>
2018-08-25 12:20:37 +0100

    further build development

--- gini_calc.R
+++ gini_calc.R
@@ -1,71 +1,45 @@
-# Packages ----------------------------------------------------------------
-
-library(tidyr)
-library(dplyr)
-library(ggplot2)
-
-
-# 1. Splitting Calc -------------------------------------------------------
-
-subdata <- iris %>% 
-  filter(Species %in% c("setosa", "virginica")) %>%
-  dplyr::select(Species, Sepal.Length) %>%
-  droplevels()
-
-head(subdata)
-
-
-# Variables ---------------------------------------------------------------
-
-kObs <- nrow(subdata) # Number of observations in the data set
-kResponseVals <- unique(subdata$Species) # Number of unique response values to classify
-
+calculateCostGINI <- function(x){
   
-# GINI INDEX --------------------------------------------------------------
-
-# Calculate cost before splitting
-cost.before <- 1 - subdata %>% 
-  group_by(Species) %>%
-  summarise(gini = (n() / kObs)^2) %>%
-  ungroup() %>%
-  summarise(gini = sum(gini))
-
-
-# Calculate cost for all unique splits in Sepal.Length
-cost.vector <- c(rep(0,length(unique(subdata$Sepal.Length))))
-
-for(i in 1:length(unique(subdata$Sepal.Length))){
+  cost.before <- 1 - x %>% 
+    group_by(Response) %>%
+    summarise(gini = (n() / nrow(x))^2) %>%
+    ungroup() %>%
+    summarise(gini = sum(gini))
   
-  # Calculate cost for subset of data containing rows less than or equal to split value
-  x1 <- subdata[subdata$Sepal.Length <= unique(subdata$Sepal.Length)[i],]
-
-  cost1 <- 1 - (x1 %>% 
-                   group_by(Species) %>%
-                   summarise(gini = (n() / nrow(x1))^2) %>%
-                   ungroup() %>%
-                   summarise(gini = sum(gini)))[[1]]
-
+  cost.vector <- c(rep(0,length(unique(x$Predictor))))
   
-  # Calculate cost for subset of data containing rows greater than split value
-  x2 <- subdata[subdata$Sepal.Length > unique(subdata$Sepal.Length)[i],]
+  for(i in 1:length(unique(x$Predictor))){
+    
+    # Calculate cost for subset of data containing rows less than or equal to split value
+    x1 <- x[x$Predictor <= unique(x$Predictor)[i],]
+    
+    cost1 <- 1 - x1 %>% 
+      filter(Predictor <= unique(x$Predictor)[i]) %>%
+      group_by(Response) %>%
+      summarise(gini = (n() / nrow(x1))^2) %>%
+      ungroup() %>%
+      summarise(gini = sum(gini))
+    
+    
+    # Calculate cost for subset of data containing rows greater than split value
+    x2 <- x[x$Predictor > unique(x$Predictor)[i],]
+    
+    cost2 <- 1 - x2 %>%
+      group_by(Response) %>%
+      summarise(gini = (n() / nrow(x2))^2) %>%
+      ungroup() %>%
+      summarise(gini = sum(gini))
+    
+    
+    # Weight by probabilities
+    cost <- ((nrow(x1)/kObs) * cost1) + ((nrow(x2)/kObs) * cost2)
+    
+    # Update cost vector with cost value for current split
+    cost.vector[i] <- cost[[1]]
+    
+  }
   
-  cost2 <- 1 - (x2 %>% 
-                    group_by(Species) %>%
-                    summarise(gini = (n() / nrow(x2))^2) %>%
-                    ungroup() %>%
-                    summarise(gini = sum(gini)))[[1]]
-
+  output <- data.frame(cost.value = min(cost.vector),
+                       split.value = unique(x$Predictor)[which.min(cost.vector)])
   
-  # Weight by probabilities
-  cost <- ((nrow(x1)/kObs) * cost1) + ((nrow(x2)/kObs) * cost2)
-  
-  # Update cost vector with cost value for current split
-  cost.vector[i] <- cost
-  
-}
-
-cost.vector
-
-min(cost.vector)
-
-
+}  
