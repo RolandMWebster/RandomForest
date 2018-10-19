f95cdcf63809d6683248c77577b03d050116a02a
RolandMWebster <39658855+RolandMWebster@users.noreply.github.com>
2018-10-19 13:47:59 +0100

    Update randomForestPrediction.R

--- randomForestPrediction.R
+++ randomForestPrediction.R
@@ -6,37 +6,38 @@
 
 
 
-# Prediction Decision Tree Function ---------------------------------------
+# Prediction Function -----------------------------------------------------
 
 predictDecisionTree <- function(data, model){
+
+  # Our model is in a nested list structure.
+  # We can loop through the model and strip away layers of the model at each step until we reach a prediciton.
+  # A $split.predictor element will only exist if the data gets split at the current node.
   
+  # Let's begin...
   
-  # We're looking for an output of the form:
-  # PREDICTION = X
-  # PROBABILITY = Y
-  
-  # We'll call the function recursively so we can keep y for all layers.
-  
-  # If a split didn't occur in the model, then we assign the output value:
+  # First check if a $split.predictor element exists in the current node
   if(is.null(model$split.predictor)){
     
+    # If it doesn't, assign the prediction output (and we're done)
     prediction <- model
     
   }else{
     
-    # If the cost reduction is large enough that we split the data then we need to determine which data set to go to by using our input value
-    # Get our split.predictor and split.value
+    # If we get here then a split must occur at the current node.
+    # We pull the split information:
     split.predictor <- model$split.predictor
     split.value <- model$split.value
     
-    # We determine our dataset to go to:
+    # We use this information to determine which branch to go down:
     if(data[,split.predictor][[1]] <= split.value){
-      model <- model$data.1
+      model <- model$data.1 
     }else{
       model <- model$data.2
     }
+    # Our model has now been stripped down to the relevant branch
     
-    # Once we've reassigned y to be our chosen subdata set, we can recursively call our function
+    # Once we've reassigned y to be our chosen subdata set, we can recursively call our function:
     predictDecisionTree(data, model)
     
   }
